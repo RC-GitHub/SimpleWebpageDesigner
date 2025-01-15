d44737f529c7a91fcfb11f5c681b5ba438f6b188
Rafał Cupał <r_cupal@inf15.local>
2025-01-15 09:09:44 +0100

    Można ustawić własną ścieżkę, usuwać projekty i pierwsze przymiarki do datagrida

---
+++ SWD/SWD/GridWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SWD
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GridWindow.xaml
+    /// </summary>
+    public partial class GridWindow : Window
+    {
+        public GridWindow(string path)
+        {
+
+            InitializeComponent();
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files) {
+                if (File.Exists(file))
+                {
+                    Debug.WriteLine(file);
+                }
+            }
+            Debug.WriteLine(files);
+        }
+    }
+}
