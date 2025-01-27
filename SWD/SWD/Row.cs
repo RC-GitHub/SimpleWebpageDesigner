6890571586339b00b792d0be27b9d4b76138818f
RC-GitHub <152216887+RC-GitHub@users.noreply.github.com>
2025-01-27 23:59:24 +0100

    Naprawiono błąd z wstawianiem większej ilości kolumn niż 12. Wprowadzono możliwość ustawiania obrazków w poszczególnych komórkach za pomocą prawego przycisku myszy.

--- SWD/SWD/Row.cs
+++ SWD/SWD/Row.cs
@@ -1,14 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace SWD
 {
     internal class Row
     {
         public string Title { get; set; }
-        public List<string> Content { get; set; }
+        public List<Cell> Content { get; set; }
+    }
+
+    internal class Cell
+    {
+        public string Title { get; set; }
+        public BitmapImage ImageSource { get; set; }
     }
 }
